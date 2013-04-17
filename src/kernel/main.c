b009b2687d4fa106b88033e13cf28c410cfd4585
yylyyl <yangyiliang@gmail.com>
2013-04-17 23:39:15 +0800

    vfprintf test added

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -10,6 +10,12 @@ os_init(void) {
 	init_idt();
 	init_i8259();
 	printk("The OS is now working!\n");
+	printk("vfprintf test starts!\n");
+	printk("test %%d: %d %d(negtive)\n", 9876654, -981234);
+	printk("test %%x: %x %x\n", 9876654, 981234);
+	printk("test %%s: %s %s(empty)\n", "hello~~~", "");
+	printk("test %%c: %c %c %c\n", 'H', 'c', '9');
+	printk("vfprintf test ends!\n");
 	sti();
 	while (TRUE) {
 		wait_intr();
